e2ea0da2ad5dd6d74f6fe278db0d92eb8ecbc2dd
Troy <troyjmcl85@gmail.com>
2019-09-09 22:04:02 -0600

    Got Schema written, fixed react bugs by binding this

--- database/index.js
+++ database/index.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
-
+//needs prop to sort repo list handle real time updates
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  user_name: String,
+  github_id: Number,
+  avatar_url: String,
+  repo_name: String,
+  repo_id: Number,
+  repo_url: String,
+  size: Number,
+  star_gazers: Number,
+  watchers: Number,
+  forks_count: Number,
+  open_issues: Number,
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
@@ -14,3 +24,7 @@ let save = (/* TODO */) => {
 }
 
 module.exports.save = save;
+
+//array of objects
+//run through access each object
+  //grab values looking for add to each instance to write to db
--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose')
 let app = express();
+
+// mongoose.connect('mongodb://localhost/github', {useNewUrlParser: true});
+
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log('connected to db')
+// });
 
 app.use(express.static(__dirname + '/../client/dist'));
 
